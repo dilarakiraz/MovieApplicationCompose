9e8e67fc88818fd461789a4043362a6d9cb5719e
dilarakiraz <dilarakiraz12345@gmail.com>
2024-10-15 14:06:20 +0300

    added to navigation and home screen

--- app/src/main/java/com/example/movieapplicationcompose/MainActivity.kt
+++ app/src/main/java/com/example/movieapplicationcompose/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.movieapplicationcompose
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -14,8 +15,13 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.movieapplicationcompose.navigation.Navigation
 import com.example.movieapplicationcompose.ui.theme.MovieApplicationComposeTheme
 import com.example.movieapplicationcompose.viewModel.MovieViewModel
 
@@ -25,6 +31,21 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MovieApplicationComposeTheme {
+                WindowCompat.setDecorFitsSystemWindows(window, false)
+                window.setFlags(
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+                )
+                val linearGradientBrush = Brush.linearGradient(
+                    colors = listOf(
+                        Color(0xFFB226E1),
+                        Color(0xFFFC6603),
+                        Color(0xFF5995EE),
+                        Color(0xFF3D3535)
+                    ),
+                    start = Offset(Float.POSITIVE_INFINITY, 0f),
+                    end = Offset(0f, Float.POSITIVE_INFINITY),
+                )
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -32,8 +53,12 @@ class MainActivity : ComponentActivity() {
 
                     val movieViewModel = viewModel<MovieViewModel>()
                     val state = movieViewModel.state
-
-                    Text(text = state.movies.toString())
+                    Box(modifier = Modifier
+                        .fillMaxSize()
+                        .background(linearGradientBrush)
+                    ) {
+                        Navigation()
+                    }
                 }
             }
         }
---
+++ app/src/main/java/com/example/movieapplicationcompose/navigation/NavHost.kt
@@ -0,0 +1,19 @@
+package com.example.movieapplicationcompose.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "Banner screen") {
+        composable("Banner screen") {
+            BannerScreen(navController = navController)
+        }
+        composable("Home screen") {
+            HomeScreen(navController = navController)
+        }
+    }
+}
